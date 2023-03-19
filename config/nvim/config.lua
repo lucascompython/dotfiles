45f49d8974e2a83eea549540b1822cc93d3340b7
Lucas de Linhares <lucasdelinhares@gmail.com>
2023-03-19 00:30:51 +0000

    Add Rainbow brackets to neovim

--- config/nvim/config.lua
+++ config/nvim/config.lua
@@ -44,7 +44,21 @@ doom.use_package({
   "github/copilot.vim"
 })
 
-
+doom.use_package({
+  "HiPhish/nvim-ts-rainbow2",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      rainbow = {
+        enable = true,
+        -- list of languages you want to disable the plugin for
+        -- disable = { "html", "lua" },
+        query = "rainbow-parens",
+        -- Highlight the entire buffer all at once
+        strategy = require("ts-rainbow.strategy.global"),
+      },
+    })
+  end,
+})
 
 
 
