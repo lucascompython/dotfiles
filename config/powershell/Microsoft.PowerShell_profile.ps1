91e57f911e5eeaf47896e9ee14dfd30ff628e31f
Lucas <77930083+lucascompython@users.noreply.github.com>
2023-08-16 23:32:45 +0100

    Update Zoxide and which command in Powershell

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 Clear-Host # To remove the annoying initial text when openning pwsh
-oh-my-posh init pwsh --config C:\Users\lucas\AppData\Local\Programs\oh-my-posh\themes/1_shell.omp.json | Invoke-Expression
+#oh-my-posh init pwsh --config C:\Users\lucas\AppData\Local\Programs\oh-my-posh\themes/1_shell.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config C:\Users\lucas\AppData\Local\Programs\oh-my-posh\themes/custom.omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
@@ -16,24 +17,21 @@ Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 #CLICK F2 to change the view
 Set-PSReadLineOption -EditMode Windows
 
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell --cmd cd | Out-String)
-})
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) }) # Zoxide
 
 # Save the current line in the history but doesn't execute it
 Set-PSReadLineKeyHandler -Key Alt+w `
-                         -BriefDescription "SaveInHistoy" `
-                         -LongDescription "Save the current line in the history but do not execute" `
-                         -ScriptBlock {
-                            param($key, $arg)
+  -BriefDescription "SaveInHistoy" `
+  -LongDescription "Save the current line in the history but do not execute" `
+  -ScriptBlock {
+  param($key, $arg)
 
-                            $line = $null
-                            $cursor = $null
-                            [Microsoft.Powershell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-                            [Microsoft.Powershell.PSConsoleReadLine]::AddToHistory($line)
-                            [Microsoft.Powershell.PSConsoleReadLine]::RevertLine()
-                         }
+  $line = $null
+  $cursor = $null
+  [Microsoft.Powershell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+  [Microsoft.Powershell.PSConsoleReadLine]::AddToHistory($line)
+  [Microsoft.Powershell.PSConsoleReadLine]::RevertLine()
+}
 Set-Alias python3 py
 Set-Alias nodejs node
 Set-Alias python py
@@ -41,14 +39,26 @@ Set-Alias csi "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\C
 Set-Alias fsi "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsi.exe"
 Set-Alias csc "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\bin\Roslyn\csc.exe"
 Set-Alias fsc "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\Tools\fsc.exe"
-Set-Alias which get-command
 
 
-function yta-mp3 {
+function global:yta-mp3 {
   yt-dlp --extract-audio --audio-format mp3 $args
-
 }
+function global:which ([Parameter(Mandatory = $true)][string] $cmd) {
 
+  $path = Get-Command $cmd 2> $null
+
+  if ($null -eq $path) { 
+    Write-Error "Command '$cmd' not found."
+    return 
+  }
+
+  if ($path.Path.Length -gt 0) {
+    return $path.Path
+  }
+  # if an alias get the location that the alias is pointing to
+  return Split-Path $path.Definition -Parent 
+}
 
 
 
@@ -61,4 +71,3 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-
