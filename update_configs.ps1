331c2649438a2a3cedd4eed1939c919b353d014a
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-07-29 19:17:02 +0100

    Start windows config

---
+++ update_configs.ps1
@@ -0,0 +1,9 @@
+# cp ~./.config/yasb/{config.yaml;styles.css} ./windows/yasb/
+
+# copy those two files to the windows directory
+Copy-Item -Path "$HOME/.config/yasb/config.yaml" -Destination "./windows/yasb/config.yaml" -Force
+Copy-Item -Path "$HOME/.config/yasb/styles.css" -Destination "./windows/yasb/styles.css" -Force
+
+Copy-Item -Path "$HOME/.config/whkdrc" -Destination "./windows/whkdrc" -Force
+
+Copy-Item -Path "$HOME/komorebi.json" -Destination "./windows/komorebi.json" -Force
