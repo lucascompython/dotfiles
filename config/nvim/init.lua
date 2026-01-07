01f866fbd8f783635a11e1ce388502f7711cd6d3
Lucas de Linhares <lucasdelinhares@gmail.com>
2026-01-07 15:02:33 +0000

    change prompt, and highlight on yank

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,15 +5,17 @@ vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
 vim.keymap.set('n', '<leader>t', ':terminal<CR>')
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
 
 -- https://github.com/xb-bx/editable-term.nvim
--- TODO: see undo/redo
-require("editable-term").setup({})
+require("editable-term").setup({
+	prompts = {
+		["^lucas@melhorpcnaoha:.* %$ "] = {}, -- risto theme
+	},
+	--wait_for_keys_delay = 50
+})
 
--- TODO: .zshrc
-    --preexec() {
-        --printf "\033]133;A\007"
-    --}
-    --precmd() {
-        --printf "\033]133;B\007"
-    --}
