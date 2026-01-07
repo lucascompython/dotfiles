7102e2e3ff58bbb2e7bde1b320b61a9ff7beb419
Lucas de Linhares <lucasdelinhares@gmail.com>
2026-01-07 15:18:18 +0000

    remove the status line

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -16,6 +16,8 @@ vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+vim.opt.laststatus = 0
+
 
 -- https://github.com/xb-bx/editable-term.nvim
 require("editable-term").setup({
